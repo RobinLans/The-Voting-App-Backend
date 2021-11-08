a2eefdb89203f6504db6a87ac230729f4d95243c
Robin <roblan95@icloud.com>
2021-11-08 16:11:14 +0100

    add endpoints to get polls

--- index.js
+++ index.js
@@ -14,16 +14,58 @@ const db = new Pool({
 app.use(cors());
 app.use(express.json());
 
-app.get("/bajs", (req, res) => {
-  db.query("SELECT * FROM test;", (err, result) => {
+app.get("/get-polls", (req, res) => {
+  db.query("SELECT * FROM polls;", (err, result) => {
     if (err) {
       console.log(err);
     }
     const data = JSON.stringify(result.rows);
+    console.log(data[0]);
     res.json(JSON.parse(data));
   });
+});
+
+app.get("/get-poll/:id", (req, res) => {
+  const pollId = req.params.id;
+
+  db.query(
+    "SELECT * FROM public.polls WHERE id = $1;",
+    [pollId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      const data = JSON.stringify(result.rows);
+
+      res.json(JSON.parse(data));
+    }
+  );
 });
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server has started");
 });
+
+// let poll = {
+//   id: 1,
+//   question: "What is the best framework?",
+//   options: ["React", "Svelte", "Vue", "Angular"],
+//   pollCount: 20,
+//   optionsWeight: [10, 2, 4, 4],
+// };
+
+// let user = {
+//   id: 1,
+//   username: "blabla",
+//   password: "pwd123",
+//   answers: [
+//     {
+//       pollId: 1,
+//       pollAnswer: 2,
+//     },
+//     {
+//       pollId: 2,
+//       pollAnswer: 4,
+//     },
+//   ],
+// };
